a3c7c5db3330a27cc82828d21657fc6a42079876
MIke <m_calix@encs.concordia.ca>
2020-02-24 19:37:05 -0500

    Logic for getting current Location

--- app/src/main/java/com/conupods/MapsActivity.java
+++ app/src/main/java/com/conupods/MapsActivity.java
@@ -7,22 +7,29 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final String TAG = "MapsActivity";
     private static final int ERROOR_DIALOG_REQUEST = 9001;
+    private static final float DEFAULT_ZOOM = 15f;
 
 
     private GoogleMap mMap;
@@ -38,12 +45,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     Boolean permissionsGranted = false;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
+    //Providers
+    //TODO Might need to update to more recent version
+    private FusedLocationProviderClient fusedLocationProvider;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        getLocationPermission();
+        if(!permissionsGranted)
+           getLocationPermission();
+
+        if(permissionsGranted)
+            getDeviceCurrentLocation();
+
 
     }
 
@@ -56,7 +72,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         locationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getCurrentLocation();
+                getDeviceCurrentLocation();
             }
         });
 
@@ -83,9 +99,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng montreal = new LatLng(45.5017, -73.561668);
-        mMap.addMarker(new MarkerOptions().position(montreal).title("Marker in Montreal"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(montreal));
+
+        Toast.makeText(this, "Maps is ready", Toast.LENGTH_SHORT).show();
     }
 
     private void getLocationPermission(){
@@ -133,8 +148,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-    private void getCurrentLocation(){
-        //TODO Logic for getting users locations IF PERMISSIONS AVAILABLE
+    private void getDeviceCurrentLocation(){
+        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this);
+
+        try{
+            if(permissionsGranted){
+                final Task currentLocation = fusedLocationProvider.getLastLocation();
+
+                currentLocation.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if(task.isSuccessful()){
+                            Log.d(TAG, "onComplete: Got the current location");
+                            Location location = (Location) currentLocation.getResult();
+
+                            moveCamera(new LatLng(location.getLatitude(), location.getLongitude()), DEFAULT_ZOOM);
+                        }
+                        else{
+                            Log.d(TAG, "onComplete: Current Location is null");
+                            Toast.makeText(MapsActivity.this, "Current location not found", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        }catch(SecurityException e){
+            Log.e(TAG, "getDeviceCurrentLOcation: SecurityException: "+ e.getMessage());
+        }
+    }
+
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.d(TAG, "initiMap: Moving the camera to Latitude: "+ latLng.latitude + " and longitude: "+ latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+
     }
 
 
