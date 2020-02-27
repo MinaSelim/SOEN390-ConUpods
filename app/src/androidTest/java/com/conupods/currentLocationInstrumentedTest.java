b15c74325256af4c4d22b38d9ab508bb87861cfa
MIke <m_calix@encs.concordia.ca>
2020-02-27 15:45:03 -0500

    Create Instrumentation Tests
    INstrumentation tests test the navigation to the MapsActivity Activity as well as inputting search test

---
+++ app/src/androidTest/java/com/conupods/currentLocationInstrumentedTest.java
@@ -0,0 +1,38 @@
+package com.conupods;
+
+import android.view.KeyEvent;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import androidx.test.espresso.action.ViewActions;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+public class currentLocationInstrumentedTest {
+
+    private String stringToBetyped;
+
+    @Before
+    public void initString(){
+        stringToBetyped = "Location Desired";
+    }
+
+    @Test
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        onView(withId(R.id.search))
+                .perform(typeText(stringToBetyped), closeSoftKeyboard())
+                .perform(ViewActions.pressKey(KeyEvent.KEYCODE_ENTER));
+
+
+        // Check that the text was changed.
+        onView(withId(R.id.search))
+                .check(matches(withText(stringToBetyped)));
+    }
+}
