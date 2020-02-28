887de1e14f808bc859d823abc6ba7e70f8c90415
Unknown <labbecarlita@gmail.com>
2020-02-27 21:30:58 -0500

    SGW campus overlay mapped

--- app/src/main/java/com/conupods/MapsActivity.java
+++ app/src/main/java/com/conupods/MapsActivity.java
@@ -1,7 +1,11 @@
 package com.conupods;
 
 import androidx.fragment.app.FragmentActivity;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.conupods.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,19 +14,38 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.data.Feature;
+import com.google.maps.android.data.geojson.GeoJsonFeature;
+import com.google.maps.android.data.geojson.GeoJsonLayer;
+import com.google.maps.android.data.geojson.GeoJsonPolygonStyle;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LatLng sgw = new LatLng(45.496080, -73.577957);
+    private LatLng loy = new LatLng(45.458333, -73.640450);
+
+    private LatLng current = sgw;
+
+    private final static String mLogTag = "GeoJsonOverlay";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -41,7 +64,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(current));
+        retrieveFileFromUrl();
+
     }
+
+
+
+
+    private void addColorsToMarkers(GeoJsonLayer layer) {
+        // Iterate over all the features stored in the layer
+        for (GeoJsonFeature feature : layer.getFeatures()) {
+            // Check if the magnitude property exists
+            if (feature.hasProperty("stroke") && feature.hasProperty("stroke-width") && feature.hasProperty("stroke-opacity")&& feature.hasProperty("fill")&& feature.hasProperty("fill-opacity")) {
+                // double magnitude = Double.parseDouble(feature.getProperty("mag"));
+                GeoJsonPolygonStyle buildingStyle = layer.getDefaultPolygonStyle();
+                // buildingStyle.setStrokeColor(Integer.parseInt(feature.getProperty("stroke")));
+                // buildingStyle.setFillColor(Integer.parseInt(feature.getProperty("fill")));
+                // buildingStyle.setStrokeColor();
+                buildingStyle.setFillColor(0x80eac700);
+                buildingStyle.setStrokeColor(0x80555555);
+                buildingStyle.setStrokeWidth(5);
+
+
+            }
+        }
+    }
+
+
+
+    private void retrieveFileFromUrl() {
+        new DownloadGeoJsonFile().execute(getString(R.string.geojson_url));
+    }
+
+    private class DownloadGeoJsonFile extends AsyncTask<String, Void, GeoJsonLayer> {
+
+        @Override
+        protected GeoJsonLayer doInBackground(String... params) {
+            try {
+                // Open a stream from the URL
+                InputStream stream = new URL(params[0]).openStream();
+
+                String line;
+                StringBuilder result = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+                while ((line = reader.readLine()) != null) {
+                    // Read and save each line of the stream
+                    result.append(line);
+                }
+
+                // Close the stream
+                reader.close();
+                stream.close();
+
+                return new GeoJsonLayer(mMap, new JSONObject(result.toString()));
+            } catch (IOException e) {
+                Log.e(mLogTag, "GeoJSON file could not be read");
+            } catch (JSONException e) {
+                Log.e(mLogTag, "GeoJSON file could not be converted to a JSONObject");
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(GeoJsonLayer layer) {
+            if (layer != null) {
+                addGeoJsonLayerToMap(layer);
+            }
+        }
+    }
+
+    private void addGeoJsonLayerToMap(GeoJsonLayer layer) {
+
+        addColorsToMarkers(layer);
+        layer.addLayerToMap();
+        // Demonstrate receiving features via GeoJsonLayer clicks.
+        layer.setOnFeatureClickListener(new GeoJsonLayer.GeoJsonOnFeatureClickListener() {
+            @Override
+            public void onFeatureClick(Feature feature) {
+                Toast.makeText(MapsActivity.this,
+                        "Feature clicked: " + feature.getProperty("title"),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
 }
