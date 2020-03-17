58e60e9527beb9d0759fa99ba75fa119a8abb07f
FinnDavidson <fintandavidson@gmail.com>
2020-03-16 20:54:34 -0400

    pathOverlay.createPolyline takes an int[][] as an input
    the polyline will be generated from a given input of coordinates that are being submitted as an interger double array

--- app/src/main/java/com/conupods/IndoorMaps/View/PathOverlay.java
+++ app/src/main/java/com/conupods/IndoorMaps/View/PathOverlay.java
@@ -27,10 +27,10 @@ public class PathOverlay {
 
     private GoogleMap mMap;
 
-    LatLng NWHall = new LatLng(45.497161, -73.579554);
-    LatLng SWHall = new LatLng(45.496827, -73.578850);
-    LatLng SEHall = new LatLng(45.497370, -73.578337);
-    LatLng NEHall = new LatLng(45.497710, -73.579035);
+    LatLng mNWHall = new LatLng(45.497161, -73.579554);
+    LatLng mSWHall = new LatLng(45.496827, -73.578850);
+    LatLng mSEHall = new LatLng(45.497370, -73.578337);
+    LatLng mNEHall = new LatLng(45.497710, -73.579035);
     public PathOverlay(GoogleMap map)
     {
         mMap = map;
@@ -38,22 +38,24 @@ public class PathOverlay {
 
     //createPolyline will take a list of LatLng and generate a polyline between those points
     //in the order of the input list
-    public void createPolyLine(){
+    public void createPolyLine(int[][] coordinates){
+
         List<LatLng> points=new ArrayList<LatLng>();
-        points.add(NWHall);
-        points.add(new LatLng(((SEHall.latitude-SWHall.latitude)/273)*100+SWHall.latitude,((SWHall.longitude-SEHall.longitude)/273)*100+SWHall.longitude));
-        points.add(SEHall);
-        points.add(NEHall);
+
+        for (int x = 0; x < coordinates.length; x++) {
+                points.add(new LatLng(((mSEHall.latitude-mSWHall.latitude)/273)*coordinates[x][0]+mSWHall.latitude,((mSWHall.longitude-mSEHall.longitude)/273)*coordinates[x][1]+mSWHall.longitude));
+        }
+
         PolylineOptions desiredPoints = new PolylineOptions();
 
-        for (LatLng coordinates:points) {
-            desiredPoints.add(coordinates);
+        for (LatLng coordinate:points) {
+            desiredPoints.add(coordinate);
         }
         Polyline line = mMap.addPolyline(desiredPoints);
         line.setColor(Color.RED);
         line.setWidth(5);
     }
-    protected void onPostExecute(){
-        createPolyLine();
+    protected void onPostExecute(int[][] indoorPath){
+        createPolyLine(indoorPath);
     }
 }
