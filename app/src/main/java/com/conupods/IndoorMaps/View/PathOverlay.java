726199e31c04f484103538f6d3fcae1a1b79b2c6
FinnDavidson <fintandavidson@gmail.com>
2020-03-17 20:19:24 -0400

    #63 removing test code for pr
    removing the test code code that was used for testing the feature

--- app/src/main/java/com/conupods/IndoorMaps/View/PathOverlay.java
+++ app/src/main/java/com/conupods/IndoorMaps/View/PathOverlay.java
@@ -1,27 +1,14 @@
 package com.conupods.IndoorMaps.View;
 
-import android.os.Bundle;
-
-import android.content.res.Resources;
-import android.os.AsyncTask;
-import android.util.Log;
-
-import com.conupods.R;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.CameraUpdateFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
 import android.graphics.Color;
-
-import android.view.View;
-
-import com.conupods.R;
 
 public class PathOverlay {
 
@@ -31,6 +18,7 @@ public class PathOverlay {
     LatLng mSWHall = new LatLng(45.496827, -73.578850);
     LatLng mSEHall = new LatLng(45.497370, -73.578337);
     LatLng mNEHall = new LatLng(45.497710, -73.579035);
+
     public PathOverlay(GoogleMap map)
     {
         mMap = map;
@@ -48,13 +36,13 @@ public class PathOverlay {
                 points.add(new LatLng(mNWHall.latitude-(mNWHall.latitude-mSWHall.latitude)*(coordinates[x][0]/275)-(mNWHall.latitude-mNEHall.latitude)*(coordinates[x][1]/275),mNWHall.longitude-(mNWHall.longitude-mSWHall.longitude)*(coordinates[x][0])/275-(mNWHall.longitude-mNEHall.longitude)*(coordinates[x][1])/275));
         }
 
-
         PolylineOptions desiredPoints = new PolylineOptions();
 
         //add array of latlng to the path
         for (LatLng coordinate:points) {
             desiredPoints.add(coordinate);
         }
+
         Polyline line = mMap.addPolyline(desiredPoints);
         line.setColor(Color.RED);
         line.setWidth(5);
