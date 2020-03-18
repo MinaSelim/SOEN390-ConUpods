16af3cd3b0581abf1f815807ff9b82761b11e418
Felix Morin <felixmorin77@gmail.com>
2020-03-18 02:03:04 -0400

    Issue#60 Settings Tabs Layouts Done
    Finished the personal and info tabs of the settings

--- app/src/main/java/com/conupods/Settings/SettingsInfoActivity.java
+++ app/src/main/java/com/conupods/Settings/SettingsInfoActivity.java
@@ -17,28 +17,13 @@ public class SettingsInfoActivity extends AppCompatActivity{
         setContentView(R.layout.settings_page_info);
 
         Button done = findViewById(R.id.done3);
-        done.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SettingsInfoActivity.this, MapsActivity.class));
-            }
-        });
+        done.setOnClickListener(view -> startActivity(new Intent(SettingsInfoActivity.this, MapsActivity.class)));
 
         Button personalPage = findViewById(R.id.toggle3_1);
-        personalPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SettingsInfoActivity.this, SettingsPersonalActivity.class));
-            }
-        });
+        personalPage.setOnClickListener(view -> startActivity(new Intent(SettingsInfoActivity.this, SettingsPersonalActivity.class)));
 
         Button defaultPage = findViewById(R.id.toggle3_2);
-        defaultPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SettingsInfoActivity.this, SettingsActivity.class));
-            }
-        });
+        defaultPage.setOnClickListener(view -> startActivity(new Intent(SettingsInfoActivity.this, SettingsActivity.class)));
 
     }
 }
--- app/src/main/java/com/conupods/Settings/SettingsPersonalActivity.java
+++ app/src/main/java/com/conupods/Settings/SettingsPersonalActivity.java
@@ -1,8 +1,11 @@
 package com.conupods.Settings;
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.conupods.OutdoorMaps.View.MapsActivity;
 import com.conupods.R;
@@ -15,29 +18,33 @@ public class SettingsPersonalActivity extends AppCompatActivity {
 
         setContentView(R.layout.settings_page_personal);
 
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefEdit = preferences.edit();
+
         Button done = findViewById(R.id.done1);
-        done.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SettingsPersonalActivity.this, MapsActivity.class));
-            }
-        });
+        done.setOnClickListener(view -> startActivity(new Intent(SettingsPersonalActivity.this, MapsActivity.class)));
 
         Button defaultPage = findViewById(R.id.toggle1_1);
-        defaultPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SettingsPersonalActivity.this, SettingsActivity.class));
-            }
-        });
+        defaultPage.setOnClickListener(view -> startActivity(new Intent(SettingsPersonalActivity.this, SettingsActivity.class)));
 
         Button infoPage = findViewById(R.id.toggle1_2);
-        infoPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(SettingsPersonalActivity.this, SettingsInfoActivity.class));
-            }
+        infoPage.setOnClickListener(view -> startActivity(new Intent(SettingsPersonalActivity.this, SettingsInfoActivity.class)));
+
+        Button myAccount = findViewById(R.id.myAccount);
+        myAccount.setOnClickListener(view -> {
+            //Implement account connection
+            //Default email
+            prefEdit.putString(String.valueOf(R.id.email), "defaultEmail@gmail.com").apply();
+            TextView email = findViewById(R.id.email);
+            email.setText(preferences.getString(String.valueOf(R.id.email), "No Email Found"));
         });
 
+        Button linkedAccount = findViewById(R.id.linkedAccount);
+        linkedAccount.setOnClickListener(view -> {
+            //Implement linked account connection
+
+            TextView connected = findViewById(R.id.connected);
+            connected.setText("Connected");
+        });
     }
 }
