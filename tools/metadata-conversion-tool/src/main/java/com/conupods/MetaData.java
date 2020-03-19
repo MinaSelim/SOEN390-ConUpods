55374d667c553df290bdbdfab6940465a9dcc559
Dominik Ludera <domludera@gmail.com>
2020-03-19 17:42:29 -0400

    updated metadata-conversion-tool, converted all metadata to json file

--- tools/metadata-conversion-tool/src/main/java/com/conupods/MetaData.java
+++ tools/metadata-conversion-tool/src/main/java/com/conupods/MetaData.java
@@ -1,35 +1,64 @@
 package com.conupods;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Scanner;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.w3c.dom.ls.LSOutput;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MetaData {
-	
-	public static void main(String[] args)
-	{
-		
-	}
+
+    public static void main(String[] args) {
+
+        String pattern = "\\S*\\/(\\S+)-(\\S+)";
+        Pattern p = Pattern.compile(pattern);
+
+        try(Stream<Path> walk = Files.walk(Paths.get("../../app/src/main/assets/json"))){
+            List<String> result = walk.filter(Files::isRegularFile)
+                    .map(x -> x.toString()).collect(Collectors.toList());
+
+            for(String path : result){
+                System.out.println(path);
+                Matcher m = p.matcher(path);
+                if(m.find()) {
+                    try {
+                        metaToJson(path, m);
+                    } catch (IOException | ParseException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
 
     //If we can have some convention to naming the metadata.MetaData, floor and building strings can be omitted
 
-    public static void metaToJson(String path, int floor, String building) throws IOException, ParseException {
+    public static void metaToJson(String path, Matcher m) throws IOException, ParseException {
 
         Scanner sc = new Scanner(new BufferedReader(new FileReader(path)));
         String dataString = "";
-        do{
+        do {
             dataString += sc.nextLine();
-        }while(sc.hasNextLine());
+        } while (sc.hasNextLine());
 
         sc.close();
 
@@ -44,36 +73,35 @@ public class MetaData {
         HashMap<String, HashMap<Integer, Dictionary>> room = new HashMap<>();
 
 
-
-        for(int i = 0; i<dataArray.size(); i++){
+        for (int i = 0; i < dataArray.size(); i++) {
 
             JSONArray dataRow = (JSONArray) dataArray.get(i);
 
-            for(int j = 0; j<dataRow.size();j++){
+            for (int j = 0; j < dataRow.size(); j++) {
 
                 String place = (String) dataRow.get(j);
-                if(place != null && !place.equals("N/A")){
+                if (place != null && !place.equals("N/A")) {
                     int index = 0;
 
                     HashMap<Integer, Dictionary> coords = new HashMap<>();
                     Dictionary coord = new Hashtable();
                     coord.put("x", i);
-                    coord.put("y", j+1);
+                    coord.put("y", j + 1);
 
                     if (room.containsKey(place)) {
                         coords = room.get(place);
                         index = coords.size();
                     }
-                    coords.put(index,coord);
+                    coords.put(index, coord);
 
                     room.put(place, coords);
                 }
             }
         }
 
-        obj.put(floor,room);
+        obj.put(m.group(1), room);
 
-        FileWriter file = new FileWriter(path+".json");
+        FileWriter file = new FileWriter(path + ".json");
         file.write(obj.toJSONString());
         file.flush();
         file.close();
