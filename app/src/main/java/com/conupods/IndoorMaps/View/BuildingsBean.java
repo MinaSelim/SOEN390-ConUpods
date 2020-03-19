de88015cb211d6cef63d4fa594a12125311201d2
FinnDavidson <fintandavidson@gmail.com>
2020-03-19 13:39:14 -0400

    #63 implemented a buildingbean class
    the BuildingBean class will hold the four coordinates for every png that is being provided for the buildings
    
    *these are not the coordinates of the buildings themselves

---
+++ app/src/main/java/com/conupods/IndoorMaps/View/BuildingsBean.java
@@ -0,0 +1,33 @@
+package com.conupods.IndoorMaps.View;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class BuildingsBean {
+
+    final static LatLng NWHALL = new LatLng(45.497161, -73.579554);
+    final static LatLng SWHALL = new LatLng(45.496827, -73.578850);
+    final static LatLng SEHALL = new LatLng(45.497370, -73.578337);
+    final static LatLng NEHALL = new LatLng(45.497710, -73.579035);
+
+    final static LatLng NWJMSB = new LatLng(45.495361, -73.579364);
+    final static LatLng SWJMSB = new LatLng(45.495223, -73.579120);
+    final static LatLng SEJMSB = new LatLng(45.495417, -73.578822);
+    final static LatLng NEJMSB = new LatLng(45.495572, -73.579190);
+
+    final static LatLng NWCC = new LatLng(45.497710, -73.579035);
+    final static LatLng SWCC = new LatLng(45.497710, -73.579035);
+    final static LatLng SECC = new LatLng(45.497710, -73.579035);
+    final static LatLng NECC = new LatLng(45.497710, -73.579035);
+
+    final static LatLng NWVE = new LatLng(45.497710, -73.579035);
+    final static LatLng SWVE = new LatLng(45.497710, -73.579035);
+    final static LatLng SEVE = new LatLng(45.497710, -73.579035);
+    final static LatLng NEVE = new LatLng(45.497710, -73.579035);
+
+    final static LatLng NWVL = new LatLng(45.497710, -73.579035);
+    final static LatLng SWVL = new LatLng(45.497710, -73.579035);
+    final static LatLng SEVL = new LatLng(45.497710, -73.579035);
+    final static LatLng NEVL = new LatLng(45.497710, -73.579035);
+
+
+}
