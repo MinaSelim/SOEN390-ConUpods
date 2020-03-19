4d18dc8fa30af91502cd53e6c3f680036ab04eaf
Dominik Ludera <domludera@gmail.com>
2020-03-19 14:12:25 -0400

    integer float division correction

--- app/src/main/java/com/conupods/IndoorMaps/View/PathOverlay.java
+++ app/src/main/java/com/conupods/IndoorMaps/View/PathOverlay.java
@@ -38,7 +38,7 @@ public class PathOverlay {
 
     final static BuildingsBean buildings = new BuildingsBean();
 
-    final static int PIXELS = 275;
+    private final static int PIXELS = 275;
 
     public PathOverlay(GoogleMap map) {
         mMap = map;
@@ -46,56 +46,62 @@ public class PathOverlay {
 
     //createPolyline will take a list of LatLng and generate a polyline between those points
     //in the order of the input list
-    public void CreatePolyLine(String building, int floor, int[][] coordinates) {
+    public void createPolyLine(String building, int floor, int[][] coordinates) {
 
         List<LatLng> points = new ArrayList<LatLng>();
 
         //converting the provided coordinates too their latitude
         // and longitude and then adding them to an array
-        if(building.equals("H")) {
-            for (int x = 0; x < coordinates.length; x++) {
-                points.add(
-                        new LatLng(
-                                buildings.NWHALL.latitude-(buildings.NWHALL.latitude-buildings.SWHALL.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWHALL.latitude-buildings.NEHALL.latitude)*(coordinates[x][1]/PIXELS),
-                                buildings.NWHALL.longitude-(buildings.NWHALL.longitude-buildings.SWHALL.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWHALL.longitude-buildings.NEHALL.longitude)*(coordinates[x][1])/PIXELS
-                        ));
-            }
-        }
-        else if(building.equals("JMSB")) {
-            for (int x = 0; x < coordinates.length; x++) {
-                points.add(
-                        new LatLng(
-                                buildings.NWJMSB.latitude-(buildings.NWJMSB.latitude-buildings.SWJMSB.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWJMSB.latitude-buildings.NEJMSB.latitude)*(coordinates[x][1]/PIXELS),
-                                buildings.NWJMSB.longitude-(buildings.NWJMSB.longitude-buildings.SWJMSB.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWJMSB.longitude-buildings.NEJMSB.longitude)*(coordinates[x][1])/PIXELS
-                        ));
-            }
-        }
-        else if(building.equals("CC")) {
-            for (int x = 0; x < coordinates.length; x++) {
-                points.add(
-                        new LatLng(
-                                buildings.NWCC.latitude-(buildings.NWCC.latitude-buildings.SWCC.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWCC.latitude-buildings.NECC.latitude)*(coordinates[x][1]/PIXELS),
-                                buildings.NWCC.longitude-(buildings.NWCC.longitude-buildings.SWCC.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWCC.longitude-buildings.NECC.longitude)*(coordinates[x][1])/PIXELS
-                        ));
-            }
-        }
-        else if(building.equals("VE")) {
-            for (int x = 0; x < coordinates.length; x++) {
-                points.add(
-                        new LatLng(
-                                buildings.NWVE.latitude-(buildings.NWVE.latitude-buildings.SWVE.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWVE.latitude-buildings.NEVE.latitude)*(coordinates[x][1]/PIXELS),
-                                buildings.NWVE.longitude-(buildings.NWVE.longitude-buildings.SWVE.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWVE.longitude-buildings.NEVE.longitude)*(coordinates[x][1])/PIXELS
-                        ));
-            }
-    }
-        else if(building.equals("VL")) {
-            for (int x = 0; x < coordinates.length; x++) {
-                points.add(
-                        new LatLng(
-                                buildings.NWVL.latitude-(buildings.NWVL.latitude-buildings.SWVL.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWVL.latitude-buildings.NEVL.latitude)*(coordinates[x][1]/PIXELS),
-                                buildings.NWVL.longitude-(buildings.NWVL.longitude-buildings.SWVL.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWVL.longitude-buildings.NEVL.longitude)*(coordinates[x][1])/PIXELS
-                        ));
-            }
+        //
+        //We'll have to think about a template method
+        switch (building) {
+            case "H":
+                for (int x = 0; x < coordinates.length; x++) {
+                    points.add(
+                            new LatLng(
+                                    buildings.NWHALL.latitude-(buildings.NWHALL.latitude-buildings.SWHALL.latitude)*((float) coordinates[x][0]/PIXELS)-(buildings.NWHALL.latitude-buildings.NEHALL.latitude)*((float) coordinates[x][1]/PIXELS),
+                                    buildings.NWHALL.longitude-(buildings.NWHALL.longitude-buildings.SWHALL.longitude)*((float) coordinates[x][0])/PIXELS-(buildings.NWHALL.longitude-buildings.NEHALL.longitude)*((float) coordinates[x][1])/PIXELS
+                            ));
+                }
+                break;
+            case "JMSB":
+                for (int x = 0; x < coordinates.length; x++) {
+                    points.add(
+                            new LatLng(
+                                    buildings.NWJMSB.latitude-(buildings.NWJMSB.latitude-buildings.SWJMSB.latitude)*((float) coordinates[x][0]/PIXELS)-(buildings.NWJMSB.latitude-buildings.NEJMSB.latitude)*((float) coordinates[x][1]/PIXELS),
+                                    buildings.NWJMSB.longitude-(buildings.NWJMSB.longitude-buildings.SWJMSB.longitude)*((float) coordinates[x][0])/PIXELS-(buildings.NWJMSB.longitude-buildings.NEJMSB.longitude)*((float) coordinates[x][1])/PIXELS
+                            ));
+                }
+                break;
+            case "CC":
+                for (int x = 0; x < coordinates.length; x++) {
+                    points.add(
+                            new LatLng(
+                                    buildings.NWCC.latitude-(buildings.NWCC.latitude-buildings.SWCC.latitude)*((float) coordinates[x][0]/PIXELS)-(buildings.NWCC.latitude-buildings.NECC.latitude)*((float) coordinates[x][1]/PIXELS),
+                                    buildings.NWCC.longitude-(buildings.NWCC.longitude-buildings.SWCC.longitude)*((float) coordinates[x][0])/PIXELS-(buildings.NWCC.longitude-buildings.NECC.longitude)*((float) coordinates[x][1])/PIXELS
+                            ));
+                }
+                break;
+            case "VE":
+                for (int x = 0; x < coordinates.length; x++) {
+                    points.add(
+                            new LatLng(
+                                    buildings.NWVE.latitude-(buildings.NWVE.latitude-buildings.SWVE.latitude)*((float) coordinates[x][0]/PIXELS)-(buildings.NWVE.latitude-buildings.NEVE.latitude)*((float) coordinates[x][1]/PIXELS),
+                                    buildings.NWVE.longitude-(buildings.NWVE.longitude-buildings.SWVE.longitude)*((float) coordinates[x][0])/PIXELS-(buildings.NWVE.longitude-buildings.NEVE.longitude)*((float) coordinates[x][1])/PIXELS
+                            ));
+                }
+                break;
+            case "VL":
+                for (int x = 0; x < coordinates.length; x++) {
+                    points.add(
+                            new LatLng(
+                                    buildings.NWVL.latitude-(buildings.NWVL.latitude-buildings.SWVL.latitude)*((float) coordinates[x][0]/PIXELS)-(buildings.NWVL.latitude-buildings.NEVL.latitude)*((float) coordinates[x][1]/PIXELS),
+                                    buildings.NWVL.longitude-(buildings.NWVL.longitude-buildings.SWVL.longitude)*((float) coordinates[x][0])/PIXELS-(buildings.NWVL.longitude-buildings.NEVL.longitude)*((float) coordinates[x][1])/PIXELS
+                            ));
+                }
+            default:
+                // TO DO
+                break;
         }
 
         PolylineOptions desiredPoints = new PolylineOptions();
