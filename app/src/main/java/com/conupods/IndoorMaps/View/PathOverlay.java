82e1f3308ab806b9fb43f2c52e165d2a99e3a286
FinnDavidson <fintandavidson@gmail.com>
2020-03-19 13:39:14 -0400

    #63 implemented a buildingbean class
    the BuildingBean class will hold the four coordinates for every png that is being provided for the buildings
    
    *these are not the coordinates of the buildings themselves

--- app/src/main/java/com/conupods/IndoorMaps/View/PathOverlay.java
+++ app/src/main/java/com/conupods/IndoorMaps/View/PathOverlay.java
@@ -5,6 +5,9 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.LatLng;
 
+
+import com.conupods.IndoorMaps.View.BuildingsBean;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,11 +17,9 @@ public class PathOverlay {
 
     private GoogleMap mMap;
 
+    final static BuildingsBean buildings = new BuildingsBean();
+
     final static int PIXELS = 275;
-    LatLng mNWHall = new LatLng(45.497161, -73.579554);
-    LatLng mSWHall = new LatLng(45.496827, -73.578850);
-    LatLng mSEHall = new LatLng(45.497370, -73.578337);
-    LatLng mNEHall = new LatLng(45.497710, -73.579035);
 
     public PathOverlay(GoogleMap map) {
         mMap = map;
@@ -26,18 +27,56 @@ public class PathOverlay {
 
     //createPolyline will take a list of LatLng and generate a polyline between those points
     //in the order of the input list
-    public void createPolyLine(int[][] coordinates) {
+    public void CreatePolyLine(String building, int floor, int[][] coordinates) {
 
         List<LatLng> points=new ArrayList<LatLng>();
 
         //converting the provided coordinates too their latitude
         // and longitude and then adding them to an array
-        for (int x = 0; x < coordinates.length; x++) {
+        if(building.equals("H")) {
+            for (int x = 0; x < coordinates.length; x++) {
                 points.add(
                         new LatLng(
-                                mNWHall.latitude-(mNWHall.latitude-mSWHall.latitude)*(coordinates[x][0]/PIXELS)-(mNWHall.latitude-mNEHall.latitude)*(coordinates[x][1]/PIXELS),
-                                mNWHall.longitude-(mNWHall.longitude-mSWHall.longitude)*(coordinates[x][0])/PIXELS-(mNWHall.longitude-mNEHall.longitude)*(coordinates[x][1])/PIXELS
+                                buildings.NWHALL.latitude-(buildings.NWHALL.latitude-buildings.SWHALL.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWHALL.latitude-buildings.NEHALL.latitude)*(coordinates[x][1]/PIXELS),
+                                buildings.NWHALL.longitude-(buildings.NWHALL.longitude-buildings.SWHALL.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWHALL.longitude-buildings.NEHALL.longitude)*(coordinates[x][1])/PIXELS
                         ));
+            }
+        }
+        else if(building.equals("JMSB")) {
+            for (int x = 0; x < coordinates.length; x++) {
+                points.add(
+                        new LatLng(
+                                buildings.NWJMSB.latitude-(buildings.NWJMSB.latitude-buildings.SWJMSB.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWJMSB.latitude-buildings.NEJMSB.latitude)*(coordinates[x][1]/PIXELS),
+                                buildings.NWJMSB.longitude-(buildings.NWJMSB.longitude-buildings.SWJMSB.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWJMSB.longitude-buildings.NEJMSB.longitude)*(coordinates[x][1])/PIXELS
+                        ));
+            }
+        }
+        else if(building.equals("CC")) {
+            for (int x = 0; x < coordinates.length; x++) {
+                points.add(
+                        new LatLng(
+                                buildings.NWCC.latitude-(buildings.NWCC.latitude-buildings.SWCC.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWCC.latitude-buildings.NECC.latitude)*(coordinates[x][1]/PIXELS),
+                                buildings.NWCC.longitude-(buildings.NWCC.longitude-buildings.SWCC.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWCC.longitude-buildings.NECC.longitude)*(coordinates[x][1])/PIXELS
+                        ));
+            }
+        }
+        else if(building.equals("VE")) {
+            for (int x = 0; x < coordinates.length; x++) {
+                points.add(
+                        new LatLng(
+                                buildings.NWVE.latitude-(buildings.NWVE.latitude-buildings.SWVE.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWVE.latitude-buildings.NEVE.latitude)*(coordinates[x][1]/PIXELS),
+                                buildings.NWVE.longitude-(buildings.NWVE.longitude-buildings.SWVE.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWVE.longitude-buildings.NEVE.longitude)*(coordinates[x][1])/PIXELS
+                        ));
+            }
+    }
+        else if(building.equals("VL")) {
+            for (int x = 0; x < coordinates.length; x++) {
+                points.add(
+                        new LatLng(
+                                buildings.NWVL.latitude-(buildings.NWVL.latitude-buildings.SWVL.latitude)*(coordinates[x][0]/PIXELS)-(buildings.NWVL.latitude-buildings.NEVL.latitude)*(coordinates[x][1]/PIXELS),
+                                buildings.NWVL.longitude-(buildings.NWVL.longitude-buildings.SWVL.longitude)*(coordinates[x][0])/PIXELS-(buildings.NWVL.longitude-buildings.NEVL.longitude)*(coordinates[x][1])/PIXELS
+                        ));
+            }
         }
 
         PolylineOptions desiredPoints = new PolylineOptions();
@@ -50,8 +89,5 @@ public class PathOverlay {
         Polyline line = mMap.addPolyline(desiredPoints);
         line.setColor(Color.RED);
         line.setWidth(5);
-    }
-    protected void OnPostExecute(int[][] indoorPath){
-        createPolyLine(indoorPath);
     }
 }
