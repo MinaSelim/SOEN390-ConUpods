0bed626018d073187b839f299013b27e21506bca
patricia nunes <pat.nunes94@gmail.com>
2020-03-21 01:15:16 -0400

    Modified IndoorBuildingOverlays to include first floor of JMSB
    Related to issue #31

--- app/src/main/java/com/conupods/IndoorMaps/IndoorBuildingOverlays.java
+++ app/src/main/java/com/conupods/IndoorMaps/IndoorBuildingOverlays.java
@@ -19,35 +19,63 @@ public class IndoorBuildingOverlays {
 
     private GoogleMap mMap;
 
+
+    private static final LatLng Building_JMSB = new LatLng(45.49575150228435, -73.5789343714714);
+
     private static final LatLng Building_HALL = new LatLng(45.497273, -73.578955);
     private static final LatLng NEAR_Building_HALL = new LatLng(Building_HALL.latitude + 0.0005, Building_HALL.longitude - 0.0001);
 
 
     private List<BitmapDescriptor> mImages = new ArrayList<BitmapDescriptor>();
-    private GroundOverlay mGroundOverlay;
+    private List<BitmapDescriptor> mImagesJMSB = new ArrayList<BitmapDescriptor>();
+    private GroundOverlay mHALLOverlay;
+    private GroundOverlay mJMSBOverlay;
     private View mLevelButtons;
+    private View floorButtonsHall;
+    private View floorButtonsJmsb;
 
 
     public IndoorBuildingOverlays(View LevelButtons, GoogleMap map) {
         mMap = map;
         mLevelButtons = LevelButtons;
         mMap.setIndoorEnabled(false);
+
         mImages.clear();
+        //mImagesJMSB.clear();
+
+        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.h1));
+        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.h2));
         mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.h8));
         mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.h9));
+
+        mImages.add(BitmapDescriptorFactory.fromResource(R.drawable.mb1));
+
+        floorButtonsJmsb =  mLevelButtons.findViewById(R.id.floorButtonsJmsb);
+        floorButtonsHall = mLevelButtons.findViewById(R.id.floorButtonsHall);
     }
 
     public void hideLevelButton() {
-        if(mLevelButtons.getVisibility() == View.VISIBLE) {
-            mLevelButtons.setVisibility(View.GONE);
-        }
+        floorButtonsJmsb.setVisibility(View.INVISIBLE);
+        floorButtonsHall.setVisibility(View.INVISIBLE);
+        //removeOverlay();
+        /*if(mLevelButtons.getVisibility() == View.VISIBLE) {
+            mLevelButtons.setVisibility(View.INVISIBLE);
+            removeOverlay();
+        }*/
     }
 
-    public void showLevelButton() {
+    public void showLevelButton(String building) {
 
-        if(mLevelButtons.getVisibility() == View.GONE) {
-            mLevelButtons.setVisibility(View.VISIBLE);
+        if(building == "JMSB"){
+            floorButtonsJmsb.setVisibility(View.VISIBLE);
         }
+        if(building == "HALL"){
+            floorButtonsHall.setVisibility(View.VISIBLE);
+        }
+
+        //if(mLevelButtons.getVisibility() == View.INVISIBLE) {
+         //   mLevelButtons.setVisibility(View.VISIBLE);
+        //}
     }
 
     //Might be a better way to hidePOIs?
@@ -82,17 +110,70 @@ public class IndoorBuildingOverlays {
         mMap.setMapStyle(style);
     }
 
-    public void displayOverlay(int index) {
+    public void displayOverlayJMSB(){
+
         hidePOIs(1);
-        mGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
-                .image(mImages.get(index)).anchor(0, 1)
+        mJMSBOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(mImages.get(4)).anchor(0, 1)
+                .position(Building_JMSB, 70f, 70f)
+                .bearing(130));
+
+
+
+    }
+
+    public void displayOverlayHall(){
+        hidePOIs(1);
+
+        mHALLOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(mImages.get(0)).anchor(0, 1)
                 .position(NEAR_Building_HALL, 80f, 80f)
                 .bearing(124));
+
+    }
+
+
+
+
+    public void displayOverlay(String building) {
+        hidePOIs(1);
+
+        /*?boolean firstDisplay=false;
+        if (firstDisplay) {
+            mGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+                    .image(mImages.get(0)).anchor(0, 1)
+                    .position(NEAR_Building_HALL, 80f, 80f)
+                    .bearing(124));
+
+        } else {*/
+        //r
+
+
+
+    }
+
+
+    public void changeOverlay(int index, String building) {
+        hidePOIs(1);
+        //removeOverlay();
+
+        if(building.equals("JMSB")){
+            mJMSBOverlay.setImage(mImagesJMSB.get(index));
+
+        }
+        if(building.equals("HALL")) {
+            mHALLOverlay.setImage(mImages.get(index));
+        }
     }
 
     public void removeOverlay() {
-        if(mGroundOverlay != null) {
-            mGroundOverlay.remove();
+
+
+        if(mHALLOverlay!=null){
+            mHALLOverlay.setVisible(false);
+        }
+        if(mJMSBOverlay!=null){
+            mJMSBOverlay.setVisible(false);
         }
     }
 }
