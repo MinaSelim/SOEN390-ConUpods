9a604cc2feb3ae29a388db434a775e84ff144d75
mcalixte <mike.calixte@yahoo.ca>
2020-03-24 22:31:20 -0400

    Transitioning to setupSearch activity

--- app/src/main/java/com/conupods/OutdoorMaps/View/SearchSetupView/SearchSetupActivity.java
+++ app/src/main/java/com/conupods/OutdoorMaps/View/SearchSetupView/SearchSetupActivity.java
@@ -1,10 +1,14 @@
 package com.conupods.OutdoorMaps.View.SearchSetupView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.conupods.R;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class SearchSetupActivity extends AppCompatActivity {
 
@@ -12,5 +16,35 @@ public class SearchSetupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_setup_view);
+
+        // get the bottom sheet view
+        LinearLayout llBottomSheet = (LinearLayout) findViewById(R.id.bottom_sheet);
+
+        // init the bottom sheet behavior
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+
+        // change the state of the bottom sheet
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        // set the peek height
+        bottomSheetBehavior.setPeekHeight(340);
+
+        // set hideable or not
+        bottomSheetBehavior.setHideable(false);
+
+        // set callback for changes
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
     }
 }
