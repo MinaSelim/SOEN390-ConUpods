789a6b3fbbb418c5d45b3a3022f49d72544deb1d
mcalixte <mike.calixte@yahoo.ca>
2020-03-24 21:08:07 -0400

    #24-Began setup search activity

---
+++ app/src/main/java/com/conupods/OutdoorMaps/View/SearchSetupView/SearchSetupActivity.java
@@ -0,0 +1,16 @@
+package com.conupods.OutdoorMaps.View.SearchSetupView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.conupods.R;
+
+public class SearchSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_setup_view);
+    }
+}
