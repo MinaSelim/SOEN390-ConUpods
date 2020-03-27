3a3bcba0eba123289da69fd84e434a44723c6ab3
Felix Morin <felixmorin77@gmail.com>
2020-03-26 20:37:03 -0400

    #60 Tests fixed
    -Fixed broken tests and added one

--- app/src/androidTest/java/com/conupods/Settings/SettingsActivityTest.java
+++ app/src/androidTest/java/com/conupods/Settings/SettingsActivityTest.java
@@ -2,11 +2,13 @@ package com.conupods.Settings;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Utility;
 import android.view.View;
 import android.widget.CheckBox;
+
 import androidx.test.rule.ActivityTestRule;
 
-import org.junit.After;
+
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,72 +16,81 @@ import org.junit.Test;
 import com.conupods.R;
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 public class SettingsActivityTest {
-
+    private static final String TAG = "SETTINGS_ACTIVITY_TEST";
     @Rule
     public ActivityTestRule<SettingsActivity> mActivityTestRule = new ActivityTestRule<>(SettingsActivity.class);
-    private SettingsActivity mActivity = null;
 
     @Rule
     public ActivityTestRule<SettingsInfoActivity> mActivityInfoTestRule = new ActivityTestRule<>(SettingsInfoActivity.class);
-    private SettingsInfoActivity mActivityInfo = null;
 
     @Rule
     public ActivityTestRule<SettingsPersonalActivity> mActivityPersonalTestRule = new ActivityTestRule<>(SettingsPersonalActivity.class);
-    private SettingsPersonalActivity mActivityPersonal = null;
 
     @Before
     public void setUp() {
-        mActivity = mActivityTestRule.getActivity();
-        mActivityInfo = mActivityInfoTestRule.getActivity();
-        mActivityPersonal = mActivityPersonalTestRule.getActivity();
+        Utility.turnOnDeviceLocation(TAG);
     }
 
     @Test
     public void launchSettingsTest() {
+        SettingsActivity mActivity = mActivityTestRule.getActivity();
         View settingsView = mActivity.findViewById(R.id.myPreferences);
         assertNotNull(settingsView);
     }
 
     @Test
     public void launchSettingsInfoTest() {
+        SettingsInfoActivity mActivityInfo = mActivityInfoTestRule.getActivity();
         View settingsView = mActivityInfo.findViewById(R.id.contactUs);
         assertNotNull(settingsView);
     }
 
     @Test
     public void launchSettingsPersonalTest() {
+        SettingsPersonalActivity mActivityPersonal = mActivityPersonalTestRule.getActivity();
         View settingsView = mActivityPersonal.findViewById(R.id.googleCalendar);
         assertNotNull(settingsView);
     }
 
     @Test
     public void goToSettingsPersonalActivityFromSettingsActivityTest() {
+        SettingsActivity mActivity = mActivityTestRule.getActivity();
         mActivity.findViewById(R.id.toggle2_1).callOnClick();
-        assertTrue(mActivityPersonal.findViewById(R.id.email).isShown());
+        onView(withText("My account")).check(matches(isDisplayed()));
     }
 
     @Test
     public void goToSettingsInfoActivityFromSettingsActivityTest() {
+        SettingsActivity mActivity = mActivityTestRule.getActivity();
         mActivity.findViewById(R.id.toggle2_2).callOnClick();
-        assert(mActivityInfo.findViewById(R.id.contactUs).isShown());
+        onView(withText("Help")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void exitSettingsActivity() {
+        SettingsActivity mActivity = mActivityTestRule.getActivity();
+        mActivity.findViewById(R.id.done2).callOnClick();
+        onView(withText("SGW")).check(matches(isDisplayed()));
     }
 
     @Test
     public void changePreferencesTest() {
+        SettingsActivity mActivity = mActivityTestRule.getActivity();
         SharedPreferences preferences = getApplicationContext().getSharedPreferences("Preferences", Context.MODE_PRIVATE);
-        CheckBox preference = mActivity.findViewById(R.id.concordiaShuttle);
+        SharedPreferences.Editor edit = preferences.edit();
+        CheckBox preference = new CheckBox(mActivity.getApplicationContext());
+        preference.setId(0);
+        edit.putBoolean(String.valueOf(preference.getId()), true).apply();
         mActivity.changePreferences(preference);
-        assertTrue(preferences.getBoolean(String.valueOf(preference.getId()), false));
-    }
-
-    @After
-    public void tearDown() {
-        mActivity = null;
-        mActivityInfo = null;
-        mActivityPersonal = null;
+        mActivity.checkBoxIfInPreference(preference);
+        assertFalse(preference.isChecked());
     }
 
 }
