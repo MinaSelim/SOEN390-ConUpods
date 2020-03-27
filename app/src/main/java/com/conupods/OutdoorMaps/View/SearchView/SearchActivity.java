c39f49567313724745a19351631be5de0f14e305
mcalixte <mike.calixte@yahoo.ca>
2020-03-27 18:22:51 -0400

    #71-Set the current text query field with search field data

--- app/src/main/java/com/conupods/OutdoorMaps/View/SearchView/SearchActivity.java
+++ app/src/main/java/com/conupods/OutdoorMaps/View/SearchView/SearchActivity.java
@@ -13,6 +13,7 @@ import android.transition.Fade;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
 
 import com.conupods.OutdoorMaps.Services.ActivityComponentBuilder;
 import com.conupods.OutdoorMaps.Models.Building.AbstractCampusLocation;
@@ -28,6 +29,7 @@ public class SearchActivity extends FragmentActivity implements CampusLocationsA
     private final String TAG = "SEARCH_ACTIVITY";
     private AbstractCampusLocationAdapter mAdapter;
     private SearchView mSearchBar;
+    private TextView mCurrentTextQueryField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +61,7 @@ public class SearchActivity extends FragmentActivity implements CampusLocationsA
     private void initializeComponents() {
         ActivityComponentBuilder componentBuilder = new ActivityComponentBuilder();
         mSearchBar = componentBuilder.initializeSearchBarWithFocus(findViewById(R.id.searchBar), this, this, mAdapter);
-
+        mCurrentTextQueryField = findViewById(R.id.current_search_text);
         LinearLayout searchDirectioButtons = (LinearLayout) findViewById(R.id.SearchDirectionsOptions);
         searchDirectioButtons.setFocusable(true);
         searchDirectioButtons.setFocusableInTouchMode(true);
@@ -70,25 +72,18 @@ public class SearchActivity extends FragmentActivity implements CampusLocationsA
     public void onCampusLocationSelected(AbstractCampusLocation abstractCampusLocation) {
         mSearchBar.setQuery(abstractCampusLocation.getIdentifier(), false);
         mSearchBar.clearFocus();
+        mCurrentTextQueryField.setText(abstractCampusLocation.getIdentifier());
 
     }
 
     @Override
     public void onBackPressed() {
-        // close search view on back button pressed
-       /** if (!mSearchBar.isIconified()) {
-            mSearchBar.setIconified(false);
-            mSearchBar.clearFocus();
-            return;
-        } */
 
         Intent intent = new Intent(SearchActivity.this, MapsActivity.class);
         ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(SearchActivity.this, this.mSearchBar, ViewCompat.getTransitionName(this.mSearchBar));
-
         startActivity(intent, options.toBundle());
         finish();
 
-        //super.onBackPressed();
     }
 
 
