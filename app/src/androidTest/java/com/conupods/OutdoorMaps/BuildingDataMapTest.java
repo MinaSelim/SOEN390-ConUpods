0b04423d31140603ca64b3d5fccd715a1cbc8518
FinnDavidson <fintandavidson@gmail.com>
2020-03-29 12:59:14 -0400

    #31 Polishing

--- app/src/androidTest/java/com/conupods/OutdoorMaps/BuildingDataMapTest.java
+++ app/src/androidTest/java/com/conupods/OutdoorMaps/BuildingDataMapTest.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import java.util.Map;
 
