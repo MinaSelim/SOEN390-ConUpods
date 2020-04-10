2fe8a2a19b6ea1cebf25c074207522ac69780302
Mina <supermina55555@hotmail.com>
2020-04-10 17:50:37 -0400

    \#63 Modified code and showed sample on how to use get the data

--- app/src/main/java/com/conupods/IndoorMaps/ConcreteBuildings/HBuilding.java
+++ app/src/main/java/com/conupods/IndoorMaps/ConcreteBuildings/HBuilding.java
@@ -1,7 +1,7 @@
 package com.conupods.IndoorMaps.ConcreteBuildings;
 
-import com.conupods.OutdoorMaps.Models.Building.Building;
 import com.conupods.OutdoorMaps.BuildingDataMap;
+import com.conupods.OutdoorMaps.Models.Building.Building;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.util.List;
@@ -12,14 +12,30 @@ public class HBuilding extends Building {
     private static BuildingDataMap mDataInstance = BuildingDataMap.getInstance();
     private static Map<LatLng, Building> mDataMapHash = mDataInstance.getDataMap();
     private static Building instance;
+    public static final int MAX_NUMBER_OF_FLOORS = 4;
+
+
+    private HBuilding(Building building)
+    {
+        this(building.getCode(), building.getName(), building.getLongName(), building.getAddress(),
+                building.getLatLng(), building.getOverlayLatLng(), building.getClassRooms());
+    }
 
     private HBuilding(String code, String name, String longName, String address, LatLng latLng, LatLng overlayLatLng, List<String> classRooms) {
         super(classRooms,latLng, name, null, longName, address, code, overlayLatLng);
+        mFloorMetaDataGrid = new String [MAX_NUMBER_OF_FLOORS][][];
+        mTraversalBinaryGrid = new boolean [MAX_NUMBER_OF_FLOORS][][];
+        initializeGridsByFloor(0, "json/DONE/1-H", "json/BooleanArray/H1");
+        initializeGridsByFloor(1, "json/DONE/2-H", "json/BooleanArray/H2");
+        initializeGridsByFloor(2, "json/DONE/8-H", "json/BooleanArray/H8");
+        initializeGridsByFloor(3, "json/DONE/9-H", "json/BooleanArray/H9");
     }
 
-    public static Building getInstance() {
+    public static synchronized Building getInstance() {
         if (instance == null) {
-            instance = mDataMapHash.get(new LatLng(45.497092, -73.5788));
+            Building temp = mDataMapHash.get(new LatLng(45.497092, -73.5788));
+            instance = new HBuilding(temp);
+            mDataMapHash.replace(new LatLng(45.497092, -73.5788), temp, instance);
         }
         return instance;
     }
