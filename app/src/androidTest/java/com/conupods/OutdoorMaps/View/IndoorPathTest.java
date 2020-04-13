6a0451b0f5a6908d2b82075b69c311186aa68cdf
patricia nunes <pat.nunes94@gmail.com>
2020-04-13 16:15:16 -0400

    \#63 Removed imports that were not being used.
    Added comments for clarity.
    
    Related to issues: #93, #31

--- app/src/androidTest/java/com/conupods/OutdoorMaps/View/IndoorPathTest.java
+++ app/src/androidTest/java/com/conupods/OutdoorMaps/View/IndoorPathTest.java
@@ -2,19 +2,15 @@ package com.conupods.OutdoorMaps.View;
 
 import android.util.Utility;
 
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
-import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
-import com.conupods.IndoorMaps.IndoorOverlayHandlers.HallBuildingHandler;
 import com.conupods.MapsActivity;
-import com.conupods.OutdoorMaps.View.SearchView.SearchActivity;
 import com.conupods.R;
 
 import org.junit.Assert;
@@ -29,9 +25,6 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class IndoorPathTest {
     private final static String TAG = "INDOOR_PATH_TEST";
@@ -48,50 +41,43 @@ public class IndoorPathTest {
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
     }
 
-    //TODO: function to check view exists and then click view
-    public void performClick(int resource, String text){
+    //Checks the appropriate text is displayed and performs click action
+    public void performClick(int resource, String text) {
         onView(withText(text)).check(matches(isDisplayed()));
         onView(ViewMatchers.withId(resource)).perform(click());
     }
-    public void executeSearch(int resourceView, int recycler, String text){
+
+    //Types value into search bar and selects the first item within recycler
+    public void executeSearch(int resourceView, int recycler, String text) {
         onView(withId(resourceView)).perform(typeText(text));
         onView(withId(recycler))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
 
+    //Tests the indoor path between two classrooms
+    //within the same building (HALL) and the same floor (9th floor)
     @Test
-    public void startSearchActivityTest() {
+    public void pathBetweenTwoClassroomsTest() {
         String currentLocation = "H 917";
         String destination = "H 921";
 
+        //Obtain Search Bar UI object
         UiObject searchBar = mDevice.findObject(new UiSelector().resourceId(("android:id/search_bar")));
+
+        //Ensure search bar exists
         Assert.assertTrue("Search bar exists", searchBar.exists());
 
-        executeSearch(R.id.searchBar,R.id.recycler_view, destination);
-        //executeSearch(R.id.searchBar,R.id.recycler_view, destination);
-//        onView(withId(R.id.searchBar)).perform(typeText(destination));
-//        onView(withId(R.id.recycler_view))
-//                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        //Type destination in search bar
+        executeSearch(R.id.searchBar, R.id.recycler_view, destination);
 
+        //Press the get directions button
         performClick(R.id.get_directions_btn, "Get Directions");
+
+        //Change current location to a classroom on 9th floor of H Building
         performClick(R.id.modeSelect_from, "From: Current Location");
+        executeSearch(R.id.fromSearchBar, R.id.recycler_view, currentLocation);
 
-//        onView(withText("Get Directions")).check(matches(isDisplayed()));
-//        onView(ViewMatchers.withId(R.id.get_directions_btn)).perform(click());
-
-//        onView(withText("From: Current Location")).check(matches(isDisplayed()));
-//        onView(ViewMatchers.withId(R.id.modeSelect_from)).perform(click());
-
-        executeSearch(R.id.fromSearchBar,R.id.recycler_view, currentLocation);
-
-
-//        onView(withId(R.id.fromSearchBar)).perform(typeText(currentLocation));
-//        onView(withId(R.id.recycler_view))
-//                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-//
+        //Select walking as the mode of transportation
         performClick(R.id.modeSelect_walkingButton, "Walking");
-//        onView(withText("Walking")).check(matches(isDisplayed()));
-//        onView(ViewMatchers.withId(R.id.modeSelect_walkingButton)).perform(click());
-
     }
 }
