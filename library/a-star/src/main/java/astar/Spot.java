2c9ca98b59f09b4ee47cb07a1a5ece93d6d06037
Dominik Ludera <domludera@gmail.com>
2020-04-19 22:57:38 -0400

    update docs

--- library/a-star/src/main/java/astar/Spot.java
+++ library/a-star/src/main/java/astar/Spot.java
@@ -21,6 +21,9 @@ public class Spot {
     private List<Spot> mNeighbors;
     private Spot mPrevious;
 
+    /**
+     *
+     */
     public Spot(){
 
         this.mX = 0;
@@ -54,84 +57,170 @@ public class Spot {
 
     }
 
+    /**
+     *
+     * @param Previous
+     */
     public void setPrevious(Spot Previous) {
         this.mPrevious = Previous;
     }
 
+    /**
+     *
+     * @return previous Spot of current Spot
+     */
     public Spot getPrevious() {
         return this.mPrevious;
     }
 
+    /**
+     *
+     * @param neighbor
+     * adds neighbours to Spot list
+     */
     public void addNeighbor(Spot neighbor) {
         if (!this.equals(neighbor)) {
             this.mNeighbors.add(neighbor);
         }
     }
 
+    /**
+     *
+     * @return list of neighbors of current Spot
+     */
     public List<Spot> getNeighbors() {
         return this.mNeighbors;
     }
 
+    /**
+     *
+     * @return X coordinate
+     */
     public int getX() {
         return mX;
     }
 
+    /**
+     *
+     * @return Y coordinate
+     */
     public int getY() {
         return mY;
     }
 
+    /**
+     *
+     * @return [x,y] array
+     */
     public int[] getXY() {
         return new int[]{this.mX, this.mY};
     }
 
+    /**
+     *
+     * @param b
+     * Set boolean value for wall of current Spot
+     */
     public void setWall(boolean b) {
         this.mWall = b;
     }
 
+    /**
+     *
+     * @return checks it current point is a wall
+     */
     public boolean isWall() {
         return this.mWall;
     }
 
+    /**
+     *
+     * @return total cost of traversal
+     */
     public double getF() {
         return mF;
     }
 
+    /**
+     * updates total cost of traversal to match current G and H values
+     */
     public void updateF() {
         this.mF = this.mG + this.mH;
     }
 
+    /**
+     *
+     * @return exact cost of current path
+     */
     public double getG() {
         return mG;
     }
 
+    /**
+     *
+     * @param mG
+     * sets the exact cost of current path
+     */
     public void setG(double mG) {
         this.mG = mG;
     }
 
+    /**
+     *
+     * @return heuristic cost of the path
+     */
     public double getH() {
         return mH;
     }
 
+    /**
+     *
+     * @param mH
+     * sets heuristic cost of the path
+     */
     public void setH(double mH) {
         this.mH = mH;
     }
 
+    /**
+     *
+     * @param b
+     * @return checks if spots have same coordinates
+     */
     public boolean equals(Spot b) {
         return this.mX == b.mX && this.mY == b.mY;
     }
 
+    /**
+     *
+     * @return returns floor index
+     */
     public int getFloor() {
         return mFloor;
     }
 
+    /**
+     *
+     * @param mFloor
+     * sets floor index
+     */
     public void setFloor(int mFloor) {
         this.mFloor = mFloor;
     }
 
+    /**
+     *
+     * @return building letter acronym
+     */
     public String getBuilding() {
         return mBuilding;
     }
 
+    /**
+     *
+     * @param mBuilding
+     * sets building letter acronym
+     */
     public void setBuilding(String mBuilding) {
         this.mBuilding = mBuilding;
     }
